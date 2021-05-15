aa40728edf7f7a2150158163815bc99b42073d29
Justin Weicht <justinweicht11@gmail.com>
2021-05-15 18:28:30 -0400

    delete buttton funtionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentE1 = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -92,3 +93,20 @@ var createTaskActions = function(taskId) {
 };
 
 formE1.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+}
+
+pageContentE1.addEventListener("click", taskButtonHandler);
